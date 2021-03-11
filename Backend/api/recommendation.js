52226ee5bb6754232dcc09963a3f663ca3deeb40
JakeRoeleven <jakeroeleven44@gmail.com>
2021-03-11 22:01:09 +1100

    Pass data to python scripts

--- Backend/api/recommendation.js
+++ Backend/api/recommendation.js
@@ -1,36 +1,80 @@
 const express = require('express');
-const {spawn} = require('child_process');
+const { spawn } = require('child_process');
 const router = express.Router();
 
 router.get('/test', (req, res) => {
 
-    console.log("test called")
-    
-    var spawn = require("child_process").spawn
-    
-    console.log("spawned")
+    // Keep a track of time it takes for python to execute
+    console.time("Spawn Child Script")
+
+    // Testing data
+    let test_json_courses = {
+        "engineering": {
+            "interests": ["math", "science"],
+            "note": "Build Shit"
+        },
+        "engineering a": {
+            "interests": ["math", "science"],
+            "note": "Build Shit"
+        },
+        "engineering v": {
+            "interests": ["math", "science"],
+            "note": "Build Shit"
+        },
+        "engineering 1": {
+            "interests": ["math", "science"],
+            "note": "Build Shit"
+        },
+        "maths": {
+            "interests": ["math", "science"],
+            "note": "Build Shit"
+        },
+        "programming": {
+            "interests": ["math", "science"],
+            "note": "Build Shit"
+        },
+    }
+
+    // Looks like env path from python is py on my local machine
+    let process = spawn('py',["./api/sample.py",  'test',   'work']); 
    
-    var process = spawn('py',["./api/sample.py",  'test',   'work'] ); 
-   
-    console.log("called")
+    // Define a variable to write python data to
+    let data_out = "";
 
     try {
 
-        process.stdout.on('data', function(data) { 
-            res.send(data.toString()); 
+        // Node receives data from python
+        process.stdout.on('data', function(data) {
+            console.log(data) 
+            data_out = data.toString('utf8')
         });
 
-        process.stderr.on('data', function(data) {
-            //Here is where the error output goes
-            console.log('stderr: ' + data);
-            res.send(data.toString()); 
+        // Node receives err from python
+        // process.stderr.on('data', function(data) {
+        //     data_out += data.toString();
+        // });
+
+        // When python script ends
+        process.stdout.on('end', function(){
+
+            let python_data = data_out
+            console.log("Python Data");
+            console.log(python_data)
+            res.send(python_data);
+            console.log("-------------");
+        
         });
+
+        // Send JSON to Python
+        process.stdin.write(JSON.stringify(test_json_courses));
+        process.stdin.end();
 
     } catch (error) {
-        
         res.status(500)
-
     }
+
+    console.timeEnd("Spawn Child Script")
+
 });
 
 module.exports = router;
--- Backend/api/sample.py
+++ Backend/api/sample.py
@@ -1,7 +1,27 @@
-import sys 
-# Takes first name and last name via command  
-# line arguments and then display them 
+import sys, json, numpy, random as np
 
-print("Output from Python") 
-print("First name: " + sys.argv[1]) 
-print("Last name: " + sys.argv[2]) 
+#Read data from stdin
+def read_in():
+    
+    lines = sys.stdin.readlines()
+   
+    #Since our input would only be having one line, parse our JSON data from that
+    return json.loads(lines[0])
+
+def main():
+
+    # Get data as an array from read_in()
+    lines = read_in()
+
+    #create a numpy array
+    np_lines = np.array(lines)
+
+    #use numpys sum method to find sum of all elements in the array
+    lines_sum = np.sum(np_lines)
+
+    #return the sum to the output stream
+    print(np_lines)
+
+#start process
+if __name__ == '__main__':
+    main()
