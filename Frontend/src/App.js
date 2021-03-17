45e9e486dcbe85680c211d4cd1b557abfcc95f26
Beatrix Austria <baustria2108@gmail.com>
2021-03-17 16:08:12 +1100

    did a basic thing

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,17 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
+import './index.css';
+import StudentForm from './selectmenu/StudentForm';
 
-import SampleMenu from './sample/SampleMenu';
-import SamplePost from './sample/SamplePost';
-
-function App() {
-	
-	return ( 
-		<>
-		<SampleMenu />
-		<SamplePost />
-		</>
-	)
-
+class App extends Component {
+  render() {
+    return (
+      <div className="col-md-6">
+        <h3> Student Interest Form </h3>
+        <StudentForm />
+      </div>
+    );
+  }
 }
 
-export default App
+export default App;
+
+//didnt't wanna fuck up master sorry
