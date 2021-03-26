f6829eb8c942058e4a0d98141b82c92ec228dd45
JakeRoeleven <jakeroeleven44@gmail.com>
2021-03-27 10:54:42 +1100

    Add timer to api

--- Backend/test/test.js
+++ Backend/test/test.js
@@ -3,22 +3,25 @@ const router = express.Router();
 const {spawn} = require('child_process');
 let {PythonShell} = require('python-shell')
 
+function parseHrtimeToSeconds(hrtime) {
+    var seconds = (hrtime[0] + (hrtime[1] / 1e9)).toFixed(3);
+    return seconds;
+}
+
 router.get('/py', async (req, res) => {
 
     try {
-        console.time('Spawn Child Script');
+        let startTime = process.hrtime();
         PythonShell.run('./recommendation/subject_randomiser.py', null, function(err, results) {
             if (err) {
                 console.log(err)
                 res.status(200).send(err);
             } else {
                 let data = results;
-                console.log(data)
-                json_data = JSON.parse(data);
-                let time = console.timeEnd('Spawn Child Script');
-                json_data["run_time"] = time;
-                console.log(json_data)
-                res.status(200).json(json_data);
+                json_data = await JSON.parse(data);
+                let time = await parseHrtimeToSeconds(process.hrtime(startTime));
+                json_data["run_time"] = await time;
+                res.status(200).json(await json_data);
             }
         });
     } catch (e) {
