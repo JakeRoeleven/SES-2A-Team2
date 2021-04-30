930b74d557cdc8d4d753ecb49700f99e08478116
Jake <jakeroleeven44@gmail.com>
2021-04-30 13:36:17 +1000

    Improvments to search algorithm

--- Frontend/src/lib/searchHandler.js
+++ Frontend/src/lib/searchHandler.js
@@ -8,9 +8,14 @@ class SearchHandler {
 
     searchAllSubjects(key) {
 
-        var search = new JsSearch.Search('course_name');
-        search.addIndex('faculty');
-        search.addIndex('description');
+        if (isNaN(key)) {
+            var search = new JsSearch.Search('course_name');
+            search.addIndex('faculty');
+            search.addIndex('description');
+        } else {
+            var search = new JsSearch.Search('course_name');
+            search.addIndex('_id');
+        }
 
         Object.keys(this.subjects).forEach(elem => {
             search.addDocument(this.subjects[elem]);
--- Frontend/src/pages/Search.js
+++ Frontend/src/pages/Search.js
@@ -4,66 +4,38 @@ import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import InputBase from '@material-ui/core/InputBase';
-import {fade, makeStyles} from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import SearchIcon from '@material-ui/icons/Search';
-import Button from '@material-ui/core/Button';
 import SearchHandler from '../lib/searchHandler.js';
 import SubjectCard from '../components/SubjectCard';
+import CircularProgress from  '@material-ui/core/CircularProgress';
 
-import { AppContext } from '../AppContext';
+import {AppContext} from '../AppContext';
+
+import Paper from '@material-ui/core/Paper';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+
+import Alert from '../components/Alert';
 
 const useStyles = makeStyles((theme) => ({
     root: {
-        flexGrow: 1,
-    },
-    menuButton: {
-        marginRight: theme.spacing(2),
-    },
-    title: {
-        flexGrow: 1,
-        display: 'none',
-        [theme.breakpoints.up('sm')]: {
-            display: 'block',
-        },
-    },
-    search: {
-        position: 'relative',
-        borderRadius: theme.shape.borderRadius,
-        backgroundColor: fade(theme.palette.common.white, 0.15),
-        '&:hover': {
-            backgroundColor: fade(theme.palette.common.white, 0.25),
-        },
-        marginLeft: 0,
-        width: '100%',
-        [theme.breakpoints.up('sm')]: {
-            marginLeft: theme.spacing(1),
-            width: 'auto',
-        },
-    },
-    searchIcon: {
-        padding: theme.spacing(0, 2),
-        height: '100%',
-        position: 'absolute',
-        pointerEvents: 'none',
+        padding: '2px 4px',
         display: 'flex',
         alignItems: 'center',
-        justifyContent: 'center',
+        marginLeft: 'auto',
+        width: 400,
     },
-    inputRoot: {
-        color: 'inherit',
+    input: {
+        marginLeft: theme.spacing(1),
+        flex: 1,
     },
-    inputInput: {
-        padding: theme.spacing(1, 1, 1, 0),
-        // vertical padding + font size from searchIcon
-        paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-        transition: theme.transitions.create('width'),
-        width: '100%',
-        [theme.breakpoints.up('sm')]: {
-            width: '12ch',
-            '&:focus': {
-                width: '20ch',
-            },
-        },
+    iconButton: {
+        padding: 10,
+    },
+    divider: {
+        height: 32,
+        margin: 4,
     },
 }));
 
@@ -76,55 +48,105 @@ function Search() {
     const [results, setResults] = useState({});
     const [searchValue, setSearchValue] = useState('');
 
-    const searchSubjects = async (key) => {
-        setResults({});
-        let searchHandler = new SearchHandler(subjects);
-        let searchResults = await searchHandler.searchAllSubjects(key);
-        setResults(searchResults)
-    }
+    const [loading, setLoading] = useState(false);
+    
+    const [error, setError] = useState(false);
+    const [errorMessage, setErrorMessage] = useState('');
 
-    const onChange = (event) => {
-        setSearchValue(event.target.value);
+    // ALert related state
+    const [showAlert, setShowAlert] = useState(false);
+    const [alertMessage, setAlertMessage] = useState('');
+
+    const searchSubjects = async () => {
+
+        if (searchValue.length > 1) {
+            setLoading(true)
+            setError(false)
+            setResults({});
+            let searchHandler = new SearchHandler(subjects);
+            let searchResults = await searchHandler.searchAllSubjects(searchValue);
+            
+            setLoading(false)
+            if (Object.keys(searchResults).length < 1) {
+                setError(true)
+                setErrorMessage('No results found for: ' + searchValue)
+            } else {
+                setResults(searchResults);
+            }
+            setResults(searchResults);
+
+        } else {
+            setResults({});
+            setError(false)
+            setAlertMessage('Cannot search if input is empty!')
+            setShowAlert(true)
+        }
+
     };
 
     useEffect(() => {
-        setSubjects(data)
+        setSubjects(data);
     }, [data]);
+
+    // "Component that displays results or error messages"
+    const ResultsDisplay = () => {
+
+        if (loading) {
+            return (
+                <div style={{ paddingTop: '60px', textAlign: 'center'}}>
+                    <CircularProgress />
+                    <p> Finding Subjects related to: {searchValue} </p>
+                </div>
+
+            )
+        } if (error) {
+            return (
+                <div style={{ paddingTop: '60px', textAlign: 'center'}}>
+                    <p> {errorMessage} </p>
+                </div>
+
+            )
+        }
+        
+
+        if (Object.keys(results).length < 1) {
+            return  <p style={{textAlign: 'center'}}> Use the search bar to find subjects by name or subject code... </p>
+        } else {
+            return (
+                Object.keys(results).slice(0, 5).map((subject, key) => (
+                    <SubjectCard key={key} subject={results[subject]} />
+                ))
+            );
+        }
+
+    }
 
     return (
         <>
             <CssBaseline />
             <Container maxWidth={false}>
-            <Grid container spacing={3}>
-                <Grid item xs={8}>
-                    <Typography variant='h5'> Subject List Search </Typography>
-                </Grid>
-                <Grid item xs={4} alignItems={'flex-end'} alignContent={'flex-end'}>
-                    <div className={classes.search}>
-                        <div className={classes.searchIcon}>
-                            <SearchIcon />
+                <Grid container spacing={3}>
+                    <Grid item xs={8}>
+                        <Typography variant='h5' style={{ paddingTop: '10px' }}> Subject List Search </Typography>
+                    </Grid>
+                    <Grid item xs={4} alignItems={'flex-end'} alignContent={'flex-end'}>
+                        <div className={classes.search}>
+                            <Paper component='form' className={classes.root}>
+                                <InputBase className={classes.input} placeholder='Search Subjects' value={searchValue} onChange={(e) => setSearchValue(e.target.value)} />
+                                <Divider className={classes.divider} orientation='vertical' />
+                                <IconButton onClick={searchSubjects} className={classes.iconButton} aria-label='search'>
+                                    <SearchIcon />
+                                </IconButton>
+                            </Paper>
                         </div>
-                        <InputBase
-                            placeholder='Search…'
-                            classes={{
-                                root: classes.inputRoot,
-                                input: classes.inputInput,
-                            }}
-                            inputProps={{'aria-label': 'search'}}
-                            value={searchValue}
-                            onChange={onChange}
-                        />
-                        <Button variant="contained" onClick={() => searchSubjects(searchValue)}>Search</Button>
-                    </div>
+                    </Grid>
                 </Grid>
-            </Grid>
                 <br />
-                {Object.keys(results).slice(0, 5).map((subject, key) => (
-                    <SubjectCard key={key} subject={results[subject]} />
-                ))}
+                <ResultsDisplay />
             </Container>
+            <Alert open={showAlert} close={setShowAlert} message={alertMessage} />
         </>
-    ); 
+    );
 }
 
 export default Search;
